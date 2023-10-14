57d5b8186874a4cc1ed9950b037c93cc9894333b
elena <krimhelena2903@gmail.com>
2023-10-14 19:09:25 +0300

    add new folder task01

--- tast00_Kvadrat_chesla/Program.cs
+++ tast00_Kvadrat_chesla/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+
+/*
+0. Напишите программу,которая на входе принимает число и выдает его квадрат.
+*/
+
+System.Console.WriteLine("Введите число: ");   // cw - быстрый вызов Console.WriteLine
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+
+int result = userNumber * userNumber; // 1 способ
+System.Console.WriteLine(result); // 2 способ
+System.Console.WriteLine(userNumber*userNumber); // 3 способ
+
+
+
+
