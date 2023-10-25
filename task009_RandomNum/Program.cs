844361283b55e0ecb6aed112ec7652f52900e4ae
elena <krimhelena2903@gmail.com>
2023-10-25 19:28:49 +0300

    add all task09, task11

---
+++ task009_RandomNum/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Программа, которая выводит случайное число из отрезка [10, 3]
+78 -> 8
+12 -> 2
+85 -> 8
+*/
+
+int number = new Random().Next(10, 100);
+System.Console.WriteLine(number);
+
+int integer = number / 10;
+int decimaL = number % 10;
+
+if(integer>decimaL)
+{
+    System.Console.WriteLine("Наибольшее " + integer);
+}
+else
+{
+    System.Console.WriteLine("Наибольшее " + decimaL);
+}
---
+++ task11_sum_first_last/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Программа, которая выводит случайное трехзначное число (N) и удаляет вторую цифру этого числа (N)
+Например:
+456 -> 46
+*/
+
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine(number);
+
+int integ = number / 100;
+int doubl = number % 10;
+string integ2 = Convert.ToString(integ);
+string doubl2 = Convert.ToString(doubl);
+
+System.Console.WriteLine(integ2 + doubl2);
+
