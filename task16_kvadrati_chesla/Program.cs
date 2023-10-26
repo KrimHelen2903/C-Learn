fab253de6fa1e20f43d8f853586cf1b943988a21
elena <krimhelena2903@gmail.com>
2023-10-26 20:01:20 +0300

    add new task 16?

---
+++ task16_kvadrati_chesla/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Программа, которая принимает на вход два числа и проверяет является ли одно число квадратом другого.
+Например:
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8, 9 -> нет
+*/
+
+System.Console.WriteLine("Введите первое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (num2 == num * num)
+{
+    System.Console.WriteLine($"Число {num2} квадрат числа {num}");
+}
+if (num == num2 * num2)
+{
+    System.Console.WriteLine($"Число {num}  квадрат числа {num2}");
+}
+else if (num2>=num2)
+{
+    System.Console.WriteLine("Числа не являются квадратом");
+}
+
+
+
