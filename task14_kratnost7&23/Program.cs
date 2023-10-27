4ec6cf7725a8e1a5603c064f2cf2b85e014d8565
elena <krimhelena2903@gmail.com>
2023-10-27 14:39:01 +0300

    add new folder task14

---
+++ task14_kratnost7&23/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Программа, которая на вход принимает число и проверяет, кратно ли оно одновремено 7 и 23.
+14 ->нет
+46 ->нет
+161 ->да
+*/
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+
+    System.Console.WriteLine("Да");
+
+else
+{
+    System.Console.WriteLine("Нет");
+}
