e1f08e85dc460a102f42ac9c3d1c8031f07febf3
Elena <krimhelena2903@gmail.com>
2023-11-07 16:40:59 +0200

    add task in new example

--- new_example_prodoljenie/Program.cs
+++ new_example_prodoljenie/Program.cs
@@ -2,19 +2,49 @@
 Программа, которая принимает на вход три числа и выдает максимальное из трёх чисел. 
 */
 
-System.Console.WriteLine("Введите число: ");
-int n1 = Convert.ToInt32(Console.ReadLine());
+//System.Console.WriteLine("Введите число: ");
+//int n1 = Convert.ToInt32(Console.ReadLine());
+//
+//System.Console.WriteLine("Введите число: ");
+//int n2 = Convert.ToInt32(Console.ReadLine());
+//
+//System.Console.WriteLine("Введите число: ");
+//int n3 = Convert.ToInt32(Console.ReadLine());
+//
+//int max = n1;
+//if (n2 > max)
+//    max = n2;
+//if (n3 > max)
+//    max = n3;
+//System.Console.WriteLine("Максимальное число " + max);
 
-System.Console.WriteLine("Введите число: ");
-int n2 = Convert.ToInt32(Console.ReadLine());
+//----------------------------------------------------------------------------------------------
 
-System.Console.WriteLine("Введите число: ");
-int n3 = Convert.ToInt32(Console.ReadLine());
+/*
+Программа, которая на вход принимает число (N), а на выход выдает все четные числа от 1 до (N)
+*/
 
-int max = n1;
-if (n2 > max)
-    max = n2;
-if (n3 > max)
-    max = n3;
-System.Console.WriteLine("Максимальное число " + max);
+//System.Console.WriteLine("Введите число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//
+//for (int i = 2; i <= number; i += 2)                   //if(int %2 == 0)
+//Console.Write( i + "");
+
+//---------------------------------------------------------------------------------------------
+
+/*
+Разбор функций.
+*/
+
+//void f(int x)
+//{
+//    x +=5;
+//    Console.WriteLine(x);
+//}
+//
+//int n = 10;
+//Console.WriteLine(n);
+//f(n);
+// Console.WriteLine(n);
+
 
